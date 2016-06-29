10a4594e2d90aa521c4971fd0da31a5b4d21519d
Wojtek Siudzinski <admin@suda.pl>
2016-06-29 11:20:53 +0100

    Parametrize build/run scripts

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+IMAGE=${IMAGE_NAME:-centurylink/dray}
+
 docker run --rm \
   -v $(pwd):/src \
   -v /var/run/docker.sock:/var/run/docker.sock \
   centurylink/golang-builder:latest \
-  centurylink/dray:latest
+  $IMAGE:latest
--- run-dray.sh
+++ run-dray.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+IMAGE=${IMAGE_NAME:-centurylink/dray}
+
 docker run --name redis -d redis:latest
-docker run --name dray -d --link redis:redis -v /var/run/docker.sock:/var/run/docker.sock -p 3009:3000 centurylink/dray:latest
+docker run --name dray -d --link redis:redis -v /var/run/docker.sock:/var/run/docker.sock -p 3009:3000 $IMAGE:latest
